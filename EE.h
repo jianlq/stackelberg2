7a68c83ce2238315ac5cf6f326a2070c2c7eac08
jianlq <1427638644@qq.com>
2016-11-29 11:10:07 +0800

    energy vs delay

---
+++ EE.h
@@ -0,0 +1,255 @@
+﻿#ifndef EE_H
+#define EE_H
+#include"CGraph.h"
+#include <ilcplex/ilocplex.h>
+
+
+// 4+x^2
+double EEdictor(CGraph *G,vector<demand> & req,int ornum,double OPEN){
+	IloEnv env;
+	IloModel model(env);
+	IloCplex EEsolver(model);
+
+	int num = req.size();	
+	IloArray<IloIntVarArray> x(env, num); 
+	for(int d = 0; d < num; d++)
+		x[d] = IloIntVarArray(env, G->m, 0, 1); 	
+
+	// 对每个点进行流量守恒约束  
+	for(int d = 0; d < num; d++){
+		for(int i = 0; i < G->n; i++){    // n为顶点数
+			IloExpr constraint(env);
+			for(unsigned int k = 0; k < G->adjL[i].size(); k++) // 出度边
+				constraint += x[d][G->adjL[i][k]->id];
+			for(unsigned int k = 0; k < G->adjRL[i].size(); k++) // 入度边
+				constraint -= x[d][G->adjRL[i][k]->id];
+
+			if(i == req[d].org)
+				model.add(constraint == 1);
+			else if(i == req[d].des)
+				model.add(constraint == -1);
+			else
+				model.add(constraint == 0);
+		}
+	}
+
+	for(int i = 0; i < G->m; i++){
+		IloExpr constraint(env);
+		for(int d = 0; d <  num; d++)
+			constraint += req[d].flow*x[d][i];
+		model.add( constraint <= G->Link[i]->capacity );  
+	}
+
+	//优化目标 节能 OPEN+X^2
+	IloExpr cost(env);
+	for(int i = 0; i < G->m; i++){
+		IloExpr load(env);
+		IloIntVarArray tmp(env,num,0,1);
+		for(int d = 0; d < num; d++){
+			load += req[d].flow*x[d][i];
+			tmp[d] = x[d][i];
+		}
+		//cost += ( IloPower(load,2) + IloMax(tmp)*OPEN );
+		cost += ( load*load + IloMax(tmp)*OPEN );
+	}
+	model.add(IloMinimize(env,cost));
+
+	EEsolver.setOut(env.getNullStream());
+	double obj = INF;
+	if(EEsolver.solve()){
+		obj = EEsolver.getObjValue(); //energy
+		//delay
+		for(int i=0;i<G->m;i++){  
+			double loadc = 0;
+			for(int d=0;d < num;d++)
+				loadc += EEsolver.getValue(x[d][i])*req[d].flow;
+			G->Link[i]->latency = linearCal(loadc,G->Link[i]->capacity);
+		}
+		double del = 0;
+		for(int d=0;d < ornum;d++){
+			for(int i=0;i<G->m;i++)
+				del +=  EEsolver.getValue(x[d][i])*G->Link[i]->latency;
+		}
+		G->delay = del;
+		cout << "EE\t"<< obj <<"\t"<<del<<endl;
+	}
+	for(int i = 0; i < req.size(); i++)
+		x[i].end();
+	x.end();
+	env.end();
+	return obj;
+}
+
+double ORdictor(CGraph *G,vector<demand> eq,int ornum,double OPEN){    
+	IloEnv env;
+	IloModel model(env);
+	IloCplex ORsolver(model);
+
+	int num = eq.size();
+	IloArray<IloNumVarArray> x(env, num); 
+	for(int d = 0; d < num; d++)
+		x[d] = IloNumVarArray(env, G->m, 0, 1); // num * G->m 的二维矩阵
+
+
+	if(!CONSTANT){
+		IloNumVarArray cost(env,G->m,0.0 ,IloInfinity); 
+		IloExpr res(env);
+		for(int i=0;i<G->m;i++)
+		{
+			IloNumExpr load(env);
+			for(int d=0;d < ornum;d++)
+				load+=x[d][i]*eq[d].flow;
+			double c=G->Link[i]->capacity;
+			model.add(cost[i] >= load);
+			model.add(cost[i] >= 3*load-2*c/3);
+			model.add(cost[i] >= 10*load-16*c/3);
+			model.add(cost[i] >= 70*load-178*c/3);
+			res += cost[i];
+		}
+		model.add(IloMinimize(env, res));
+
+		//for(int i=0;i<G->m;i++){
+		//	IloExpr load(env);
+		//	for(int d=0;d<ornum;d++){ 
+		//		load += eq[d].flow*x[d][i];
+		//	}
+		//	model.add(cost[i]>=load*load/(G->Link[i]->capacity));   // [0,1/3]
+		//	model.add(cost[i]>=3.0*load*load/(G->Link[i]->capacity)-(2.0/3.0));  //[1/3,2/3]
+		//	model.add(cost[i]>=10.0*load*load/(G->Link[i]->capacity)-(16.0/3.0)); // [2/3,9/10]
+		//	model.add(cost[i]>=70.0*load*load/(G->Link[i]->capacity)-(178.0/3.0));  //[9/10,1]
+		//	model.add(cost[i]>=500*load*load/G->Link[i]->capacity-(double)(1468/3)); // [1,11/10]
+		//	model.add(cost[i]>=5000*load*load/G->Link[i]->capacity-(double)(19468/3)); //[11/10,...]
+		//   res += cost[i];	
+		//}
+		//model.add(IloMinimize(env,res));
+	}
+	else{
+		IloExpr delay(env);
+		for(int ij = 0; ij  < G->m; ij++){		
+			IloNumExpr ep1(env);
+			for(int d = 0; d < ornum; d++){
+				ep1 += x[d][ij]*G->Link[ij]->dist;  //d之和			
+			}
+			delay += ep1;		 
+		}
+		model.add(IloMinimize(env, delay)); 
+	}
+
+	//约束  流量守恒约束  对每个点进行流量守恒约束  
+	for(int d = 0; d < num; d++)
+		for(int i = 0; i < G->n; i++){    // n为顶点数
+			IloExpr constraint(env);
+			for(unsigned int k = 0; k < G->adjL[i].size(); k++) // 出度边
+				constraint += x[d][G->adjL[i][k]->id];
+			for(unsigned int k = 0; k < G->adjRL[i].size(); k++) // 入度边
+				constraint -= x[d][G->adjRL[i][k]->id];
+			// 出 - 入
+			if(i == eq[d].org)
+				model.add(constraint == 1);
+			else if(i == eq[d].des)
+				model.add(constraint == -1);
+			else
+				model.add(constraint == 0);
+		}
+
+		//带宽约束
+		for(int i = 0; i < G->m; i++){
+			IloExpr constraint(env);
+			for(int d = 0; d <  num; d++)
+				constraint += eq[d].flow*x[d][i];
+			model.add(constraint <= G->Link[i]->capacity);  
+		}
+
+		ORsolver.setOut(env.getNullStream());
+		double obj = INF;
+		ORsolver.solve();
+		if(ORsolver.getStatus() == IloAlgorithm::Infeasible)
+			env.out() << "OR Dictator No Solution" << endl;
+		else{
+			obj = ORsolver.getObjValue();
+			// 计算 energy
+			double util = 0;
+			for(int i = 0; i < G->m; i++){  
+				double load = 0;
+				double one = 0;
+				for(int d = 0; d < num; d++){
+					load += ORsolver.getValue(x[d][i])*eq[d].flow;
+					one = max(one,ORsolver.getValue(x[d][i]));
+				}
+				util += (one*OPEN + load*load);
+				//util += (one*OPEN + load);
+			}
+			G->energy = util;
+
+			// delay
+			double del = 0;
+			for(int i=0;i<G->m;i++){  
+				double loadc = 0;
+				for(int d = 0;d < ornum;d++)
+					loadc += ORsolver.getValue(x[d][i])*eq[d].flow;
+				del += linearCal(loadc,G->Link[i]->capacity);
+			}
+			obj = del;
+			cout << "OR\t"<< util<<"\t"<<obj <<endl;
+		}
+		for(int i = 0; i < eq.size(); i++)
+			x[i].end();
+		x.end();
+		env.end();
+		return obj;
+}
+
+double CGraph::EE(int id,int s,int t,double dm,bool needpath,double OPEN){
+	vector<int> p, flag;
+	vector<double> d;//energy
+	for(int i = 0; i < n; i++){
+		p.push_back(-1);
+		flag.push_back(0);
+		d.push_back(INF);
+	}
+
+	d[s] = 0;
+	int cur = s;
+	do{	
+		flag[cur] = 1;
+		for(unsigned int i = 0; i < adjL[cur].size(); i++){
+			CEdge *e = adjL[cur][i];
+			//e->latency = pow((e->use+dm),2)-pow(e->use,2);
+			e->latency = pow((e->use+dm),2);
+			if( 0 == e->use )
+				e->latency += OPEN;
+			if(e->capacity - e->use >= dm && d[e->head] > d[e->tail] + e->latency){
+				d[e->head] = d[e->tail] + e->latency;
+				p[e->head] = e->id;
+			}
+		}
+		cur = -1;
+		for(int i = 0; i < n; i++)
+			if(!flag[i] && (cur == -1 || d[cur] > d[i] ))
+				cur = i;
+	}while(cur != -1);
+
+	cur = t;
+	do{
+		if(p[cur] == -1)
+			break;
+		Link[p[cur]]->use += dm;
+		cur = Link[p[cur]]->tail;
+	}while(cur != s);
+
+	if(needpath){
+		reqPathID[id].clear();
+		cur = t;
+		do{
+			if(p[cur] == -1)
+				break;
+			this->reqPathID[id].push_back(p[cur]);
+			cur = Link[p[cur]]->tail;
+		}while(cur != s);
+		reverse(reqPathID[id].begin(),reqPathID[id].end());
+	}
+
+	return d[t];
+}
+
+#endif
---
+++ EELeader.cpp
@@ -0,0 +1,186 @@
+﻿#include"evolutionbit.h"
+#include"EE.h"
+#include"nash.h"
+
+// 在友好系数0.4下，对于不同的启动能耗，测试nash，stackelberg，EE，OR
+// 每个启动能耗下进行30个测试案例的测试，每个启动能耗最终的结果取30次的平均
+int main(){
+	srand((unsigned)time(NULL));
+	int Time = 3;
+	int CASEnum= 30;	
+	vector<double> STARTUP;
+	STARTUP.push_back(0);
+	STARTUP.push_back(10);
+	STARTUP.push_back(100);
+	STARTUP.push_back(500);
+	STARTUP.push_back(1000);
+	STARTUP.push_back(2000);
+	STARTUP.push_back(4000);
+	STARTUP.push_back(10000);
+
+	vector<double>CONSIDER;
+	int CON_VALUE = 0;
+	double c = 0.4;
+	for(int i=0;i <= CON_VALUE;i++){
+		CONSIDER.push_back(c);
+		c += 0.2;
+	}
+
+	int LOOP  = 100;
+
+	for(int i =0;i<Time;i++){
+		for(int start = 0; start < STARTUP.size();start++){
+			FILE *out = fopen("outputFile//eeor.csv", "a");
+			FILE *res = fopen("outputFile//result.csv", "a");
+			FILE *nash = fopen("outputFile//nash.csv", "a");
+
+			int conN = CONSIDER.size();
+			vector<double> see(conN,0) ;
+			vector<double> sor(conN,0);
+
+			vector<double> dicee(conN,0) ;
+			vector<double> diceeor(conN,0) ;
+
+			vector<double> dicoree(conN,0);
+			vector<double> dicor(conN,0);
+
+			vector<double> nashee(conN,0) ;
+			vector<double> nashor(conN,0);
+
+			vector<int> successCase (conN, 0) ;
+			vector<int> flag(conN,1);
+
+			for(int casenum = 0; casenum < CASEnum; casenum++){
+				genGraph(9,50,"inputFile//graph.txt");
+				genGraphOR(9,4,9,"inputFile//graphOR.txt");
+				CGraph *G = new CGraph("inputFile//graph.txt");
+				CGraph *GOR = new CGraph("inputFile//graphOR.txt");
+
+				vector<demand> eqOR;
+				vector<demand> eqTE;
+				vector<demand> eqbase;
+
+				//eqbase.clear();//background流 
+				for(int i = 0; i < BGNUM; i++){
+					int s = rand()%G->n, t;
+					do{
+						t = rand()%G->n;
+					}while( s == t || G->canNotReach(s,t));
+					eqbase.push_back(demand(s, t, rand()%(MAXDEMAND)+1));
+				}
+
+				////Overlay  产生demand
+				//eqOR.clear(); 
+				for(int i =0 ;i<GOR->m;i++)
+					if(G->canNotReach(GOR->Link[i]->tail, GOR->Link[i]->head))
+						continue;
+					else
+						eqOR.push_back(demand(GOR->Link[i]->tail,GOR->Link[i]->head,rand()%(MAXDEMAND)+1));
+
+				//eqTE.clear(); 
+				for(unsigned int i=0;i<eqOR.size();i++)
+					eqTE.push_back(eqOR[i]);
+				int ornum = eqTE.size();
+				for(unsigned int i=0;i<eqbase.size();i++)
+					eqTE.push_back(eqbase[i]);
+
+				double eedic = 0, ordic = 0;
+				G->clearOcc();
+				eedic = EEdictor(G,eqTE,ornum,STARTUP[start]);
+
+				G->clearOcc();
+				ordic = ORdictor(G,eqTE,ornum,STARTUP[start]);
+
+				if( (eedic + 1e-5 >= INF)  || ( ordic + 1e-5 >=INF) )
+					break;
+
+				G->clearOcc();
+				if(!G->GAinit(eqTE)){
+					cout << "*****GA init failed***** "<<endl;
+					break;
+				}
+
+				
+				for(int con = 0;con < CONSIDER.size();con++){
+
+					int n = 150;//种群个体数目
+					int m = eqTE.size();
+					evoluPopubit popubit(n,m,G,GOR,&eqTE,&eqOR,eedic,ordic,CONSIDER[con],STARTUP[start]);
+					(popubit).evolution();
+					cout<<"S\t"<<popubit.hero.energy<<"\t"<<popubit.hero.delay<<endl;
+
+					if( (popubit.hero.energy +1e-5) >= INF ||  (popubit.hero.delay + 1e-5) > INF ){
+						flag[con] = 0;
+						break;
+					}
+
+					//// nash	
+					int nacase = 0;
+					double loopnashee=0,loopnashor=0;
+					fprintf(out,"\n nash \n");
+					for(int i =0;i<LOOP;i++){
+						G->clearOcc();
+						double ee = NashEE(G,GOR,eqTE,STARTUP[start]);
+						if(ee + 1e-5 >= INF){
+							fprintf(nash,"NashEE unfeasible\n");
+							break;
+						}
+						double del = NashOR(GOR,eqOR);
+						if(del + 1e-5 >= INF){
+							fprintf(nash,"NashOR unfeasible\n");
+							break;
+						}
+						eqTE.clear();
+						for(int i=0;i<GOR->m;i++){
+							if(GOR->Link[i]->use>0)
+								eqTE.push_back(demand(GOR->Link[i]->tail,GOR->Link[i]->head,GOR->Link[i]->use));
+						}
+						for(unsigned int i=0;i<eqbase.size();i++)
+							eqTE.push_back(eqbase[i]);
+						GOR->clearOcc();
+						nacase++;
+						loopnashee += ee;
+						loopnashor += del;
+						fprintf(nash,"%f,%f\n",ee,del);
+					}
+					fclose(nash);
+				
+
+					if(flag[con]){	
+						fprintf(out,"EE,%f,%f,%f\n",STARTUP[start],eedic,G->delay);
+						fprintf(out,"OR,%f,%f,%f\n",STARTUP[start],G->energy,ordic);
+						fprintf(out,"S,%f,%f,%f\n",STARTUP[start],popubit.hero.energy,popubit.hero.delay);
+						fprintf(out,"Nash,%f,%f,%f\n",STARTUP[start],loopnashee/nacase,loopnashor/nacase);
+
+						successCase[con] += 1;
+						dicee[con] += eedic;
+						diceeor[con] += G->delay;
+
+						dicoree[con] += G->energy;
+						dicor[con] += ordic;						
+
+						see[con] += popubit.hero.energy;
+						sor[con] += popubit.hero.delay;
+
+						nashee[con] += (loopnashee/nacase);
+						nashor[con] += (loopnashor/nacase);
+						fclose(out);
+					}
+				}
+								
+				delete G;
+				delete GOR;
+			}
+			fprintf(res, "%f\n",STARTUP[start]);
+			for(int con = 0;con < CONSIDER.size();con++){
+				fprintf(res, "EE,%f,%f,%f\n",CONSIDER[con],dicee[con]/successCase[con],diceeor[con]/successCase[con]); 
+				fprintf(res, "OR,%f,%f,%f\n",CONSIDER[con],dicor[con]/successCase[con],dicoree[con]/successCase[con]); 
+				fprintf(res, "S,%f,%f,%f\n",CONSIDER[con],see[con]/successCase[con],sor[con]/successCase[con]); 
+				fprintf(res, "nash,%f,%f,%f\n",CONSIDER[con],nashee[con]/successCase[con],nashor[con]/successCase[con]); 
+			}
+			fclose(res);
+		}
+	}
+	system("pause");
+	return 0;	
+}
